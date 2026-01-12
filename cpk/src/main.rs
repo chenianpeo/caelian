ef6e906a3f37ecc0ee081c6277f41150c504de4d
chenian <chenianpeo@gmail.com>
2026-01-12 11:11:32 +0800

    add read application list json

--- cpk/src/main.rs
+++ cpk/src/main.rs
@@ -1,4 +1,20 @@
 use clap::{Parser, Subcommand};
+use serde::Deserialize;
+use std::fs;
+
+#[derive(Deserialize)]
+struct Package {
+    name: String,
+    version: String,
+    description: String,
+    url: String,
+    installer: String,
+}
+
+#[derive(Deserialize)]
+struct PackageRepo {
+    packages: Vec<Package>,
+}
 
 #[derive(Parser)]
 #[command(name = "cpk")]
@@ -30,13 +46,38 @@ async fn main() {
 
     match cli.command {
         Commands::List => {
-            println!("List packages - TODO");
+            let repo = load_repo();
+
+            println!("Available packages:");
+            println!("--------------------");
+
+            for pkg in &repo.packages {
+                println!(
+                    "{:<10} {:<10} {}",
+                    pkg.name,
+                    pkg.version,
+                    pkg.description
+                );
+            }
+
+            println!("--------------------");
+            println!("{} packages available", &repo.packages.len());
         }
+
         Commands::Install { name } => {
             println!("Install package: {name}");
         }
+
         Commands::Uninstall { name } => {
             println!("Uninstall packages: {name}");
         }
     }
+}
+
+fn load_repo() -> PackageRepo {
+    let data = fs::read_to_string("packages.json")
+    .expect("❌ failed to read packages.json");
+
+    serde_json::from_str::<PackageRepo>(&data)
+    .expect("❌ failed to parse packages.json")
 }

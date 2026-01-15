886869d3460955a1bd0cf3e194d51626047d3e94
chenian <chenianpeo@gmail.com>
2026-01-15 13:41:14 +0800

    using rust dll in csharp project

---
+++ cpk_chsharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    [DllImport(@"d:\Documents\Code\Projects\caelian\cpk_rust\target\debug\cpk_rust.dll", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void ffi_hello
+        ([MarshalAs(UnmanagedType.LPStr)] string name);
+
+    static void Main()
+    {
+        Console.WriteLine("Calling Rust function from C#:");
+        ffi_hello("lsr");
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
+    }
+}
--- cpk_rust/src/hello_c/hello_c.rs
+++ cpk_rust/src/hello_c/hello_c.rs
@@ -1,3 +1,6 @@
+use std::ffi::CStr;
+use std::os::raw::c_char;
+
 // write rust code
 fn hello(name: &str) {
     println!("hello {name}, welcome to cpk");
@@ -5,6 +8,15 @@ fn hello(name: &str) {
 
 // write C# code ffi api
 #[unsafe(no_mangle)]
-pub extern "C" fn ffi_hello(name: String) {
-    hello(&name);
+pub extern "C" fn ffi_hello(name: *const c_char) {
+    if name.is_null() {
+        return;
+    }
+    
+    unsafe {
+        let c_str = CStr::from_ptr(name);
+        if let Ok(rust_str) = c_str.to_str() {
+            hello(rust_str);
+        }
+    }
 }

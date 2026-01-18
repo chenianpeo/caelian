88a3cc568dc39a4bf2387f654bf01b4ec8b3c6eb
chenian <chenianpeo@gmail.com>
2026-01-18 18:58:30 +0800

    create main page

--- cael_csharp/MainWindow.xaml.cs
+++ cael_csharp/MainWindow.xaml.cs
@@ -10,9 +10,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-using YamlDotNet.Serialization;
-using YamlDotNet.Serialization.NamingConventions;
-
 namespace cael_csharp
 {
     /// <summary>
@@ -23,50 +20,6 @@ namespace cael_csharp
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Applications content = Functions.ReadYaml();
-            boxPrint.Items.Add($"Name: {content.Name}");
-            boxPrint.Items.Add($"Version: {content.Version}");
-            boxPrint.Items.Add($"Size: {content.Size}");
-            boxPrint.Items.Add($"Bytes: {content.Bytes}");
-            boxPrint.Items.Add($"Descriptions: {content.Description}");
-            boxPrint.Items.Add($"Purpose: {content.Purpose}");
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            boxPrint.Items.Clear();
-        }
-    }
-
-    public class Applications
-    {
-        public string Name { get; set; }
-        public string Version { get; set; }
-        public string Size { get; set; }
-        public string Bytes { get; set; }
-        public string Description { get; set; }
-        public string Purpose { get; set; }
-    }
-    public class Functions
-    {
-        public static Applications ReadYaml()
-        {
-            //var app = new Applications
-            //{
-            //    Name = "Abe",
-            //};
-            //var serializer = new SerializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance).Build();
-            //var yaml = serializer.Serialize(app);
-            //return yaml;
-
-            var app = File.ReadAllText("packages.yaml");
-            var deserializer = new DeserializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance).Build();
-            var test = deserializer.Deserialize<Applications>(app);
-            return test;
         }
     }
 }
